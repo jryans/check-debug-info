2462bb24e1de66e642e67a8b6d88251cb8d4536d
J. Ryan Stinnett <jryans@gmail.com>
2022-07-28 12:16:42 +0100

    Inline local variable uses in custom printing

--- lib/Module/Printing.cpp
+++ lib/Module/Printing.cpp
@@ -1,11 +1,17 @@
 #include "klee/Module/Printing.h"
 
+#include "llvm/AsmParser/Parser.h"
+#include "llvm/AsmParser/SlotMapping.h"
+#include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/Metadata.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/ModuleSlotTracker.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/Regex.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
 
+#include <cstdlib>
 #include <regex>
 #include <string>
 
@@ -45,6 +51,14 @@ std::string printModule(const Module &module) {
   out << module;
   out.flush();
 
+  // In order to inline metadata later on, we need a mapping from IR slot
+  // number to metadata node, but the IR printer treats that as private state.
+  // Luckily, the parser makes it available, so we can parse the thing we just
+  // printed to get the mapping.
+  SMDiagnostic err;
+  SlotMapping slots;
+  parseAssemblyString(str, err, module.getContext(), &slots);
+
   // Remove alignment
   std::regex alignment(", align [0-9]+");
   str = std::regex_replace(str, alignment, "");
@@ -53,13 +67,37 @@ std::string printModule(const Module &module) {
   std::regex intrinsicPrefix("call void @llvm.");
   str = std::regex_replace(str, intrinsicPrefix, "@");
 
-  // Remove "metadata"
-  std::regex metadata("metadata ");
-  str = std::regex_replace(str, metadata, "");
+  // Remove "metadata" prefix
+  std::regex metadataPrefix("metadata ");
+  str = std::regex_replace(str, metadataPrefix, "");
 
   // Remove empty `DIExpression`s
   std::regex emptyDiExpression(", !DIExpression\\(\\)");
   str = std::regex_replace(str, emptyDiExpression, "");
+
+  // Inline local variable uses
+  std::regex varMdUse(", !([0-9]+)");
+  for (std::sregex_iterator i(str.begin(), str.end(), varMdUse), e; i != e;
+       ++i) {
+    const auto &match = *i;
+    std::string slotStr = match[1].str();
+    const char *slotChars = slotStr.c_str();
+    char *slotCharsEnd;
+    unsigned slot = std::strtoul(slotChars, &slotCharsEnd, 10);
+    const auto &mdNodeIter = slots.MetadataNodes.find(slot);
+    if (mdNodeIter == slots.MetadataNodes.end())
+      continue;
+    const auto &mdNode = mdNodeIter->second;
+    if (mdNode->getMetadataID() != Metadata::DILocalVariableKind)
+      continue;
+    if (const auto *variable = cast<DILocalVariable>(mdNode)) {
+      std::string varInline;
+      raw_string_ostream varInlineOut(varInline);
+      varInlineOut << ", \"" << variable->getName() << "\"";
+      varInlineOut.flush();
+      str.replace(match.position(), match.length(), varInline);
+    }
+  }
 
   return str;
 }
