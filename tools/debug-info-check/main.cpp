e39eb38059dc9e1c405024c514e359d791d6d920
J. Ryan Stinnett <jryans@gmail.com>
2023-02-07 14:25:26 +0000

    Skip `undef` assignments when building live ranges

--- tools/debug-info-check/main.cpp
+++ tools/debug-info-check/main.cpp
@@ -489,6 +489,12 @@ void buildLiveRangeToAssignmentMap(VariablesSet &variables, VToAs &varToAs,
     for (size_t i = 0, e = assignments.size(); i < e; ++i) {
       auto &assignment = assignments[i];
 
+      // Skip `undef` assignments, as they can only close the live range
+      // Previous assignment already stops at the next assignment, so no need to
+      // look back and close it manually
+      if (assignment.varIntrinsic->isUndef())
+        continue;
+
       unsigned int startLine = assignment.startLine;
       unsigned int startColumn = assignment.startColumn;
       unsigned int endLine, endColumn;
